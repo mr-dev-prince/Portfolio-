c5ed46ec98f6edf354128716c118c20b1d349984
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-05-21 01:18:02 +0530

    refactor: Update routing in App.js to include Experiences page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Journey from "./pages/Journey";
 import Resume from "./pages/Resume";
 import Contact from "./pages/Contact";
 import { Header } from "./common/Header";
+import Experiences from "./pages/Experiences";
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/experiences" element={<Experiences />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/journey" element={<Journey />} />
         <Route path="/resume" element={<Resume />} />
